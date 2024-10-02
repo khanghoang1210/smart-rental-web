2145d0ada258366fc5ef3c0bbc77f9589e300e25
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-10-02 12:00:30 +0700

    ui: add filter page

--- src/components/filter/UtilitiesList.tsx
+++ src/components/filter/UtilitiesList.tsx
@@ -1,4 +1,3 @@
-
 import wc from "../../assets/wc.svg";
 import win from "../../assets/bed.svg";
 import wifi from "../../assets/wifi.svg";
@@ -17,52 +16,52 @@ import snow from "../../assets/snow.svg";
 import UtilitiesButton from "../../ui/UtilitiesButton";
 import { useState } from "react";
 
-
-
-
 const UtilitiesData = [
-    { id: 1, name: "WC riêng", icon: wc },
-    { id: 2, name: "Cửa sổ", icon: win },
-    { id: 3, name: "Wifi", icon: wifi },
-    { id: 4, name: "Nhà bếp", icon:kitchen },
-    { id: 5, name: "Máy giặt", icon: laundry },
-    { id: 6, name: "Tủ lạnh", icon: refri },
-    { id: 7, name: "Chỗ để xe", icon: bike },
-    { id: 8, name: "Tự do", icon: clock },
-    { id: 9, name: "Chủ riêng", icon: churieng },
-    { id: 10, name: "An ninh", icon: secure, active: true }, 
-    { id: 11, name: "Thú cưng", icon: pet, active: true }, 
-    { id: 12, name: "Gác lửng", icon: gaclung },
-    { id: 13, name: "Giường", icon: bed },
-    { id: 14, name: "Tủ đồ", icon: tudo },
-    { id: 15, name: "Máy lạnh", icon: snow },
-  ];
+  { id: 1, name: "WC riêng", icon: wc },
+  { id: 2, name: "Cửa sổ", icon: win },
+  { id: 3, name: "Wifi", icon: wifi },
+  { id: 4, name: "Nhà bếp", icon: kitchen },
+  { id: 5, name: "Máy giặt", icon: laundry },
+  { id: 6, name: "Tủ lạnh", icon: refri },
+  { id: 7, name: "Chỗ để xe", icon: bike },
+  { id: 8, name: "Tự do", icon: clock },
+  { id: 9, name: "Chủ riêng", icon: churieng },
+  { id: 10, name: "An ninh", icon: secure, active: true },
+  { id: 11, name: "Thú cưng", icon: pet, active: true },
+  { id: 12, name: "Gác lửng", icon: gaclung },
+  { id: 13, name: "Giường", icon: bed },
+  { id: 14, name: "Tủ đồ", icon: tudo },
+  { id: 15, name: "Máy lạnh", icon: snow },
+];
 const UtilitiesList = () => {
-    const [activeStates, setActiveStates] = useState(
-        UtilitiesData.reduce((acc, utility) => {
-          acc[utility.id] = false;
-          return acc;
-        }, {} as { [key: number]: boolean }) 
-      );
-    
-      const toggleActive = (id: number) => {
-        setActiveStates((prevStates) => ({
-          ...prevStates,
-          [id]: !prevStates[id], 
-        }));
-      };
-    return (
-        <div className="grid grid-cols-2 gap-4">
-          {UtilitiesData.map((utility) => (
-            <UtilitiesButton
-              key={utility.id}
-              name={utility.name}
-              icon={utility.icon}
-              active={activeStates[utility.id]} 
-              onClick={() => toggleActive(utility.id)}
-            />
-          ))}
-        </div>
-      );
-}
+  const [activeStates, setActiveStates] = useState(
+    UtilitiesData.reduce(
+      (acc, utility) => {
+        acc[utility.id] = false;
+        return acc;
+      },
+      {} as { [key: number]: boolean }
+    )
+  );
+
+  const toggleActive = (id: number) => {
+    setActiveStates((prevStates) => ({
+      ...prevStates,
+      [id]: !prevStates[id],
+    }));
+  };
+  return (
+    <div className="grid grid-cols-2 gap-4">
+      {UtilitiesData.map((utility) => (
+        <UtilitiesButton
+          key={utility.id}
+          name={utility.name}
+          icon={utility.icon}
+          active={activeStates[utility.id]}
+          onClick={() => toggleActive(utility.id)}
+        />
+      ))}
+    </div>
+  );
+};
 export default UtilitiesList;
