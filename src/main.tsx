babf2d1cfa7445931f0b7b3a5bc7519b6299a227
khanghoang1210 <nguyenhoangkhang12102003@gmail.com>
2024-11-04 12:53:06 +0700

    remove close toaster button

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ import { Toaster } from 'sonner'
 createRoot(document.getElementById('root')!).render(
   <SocketProvider>
     <App/>
-    <Toaster closeButton/>
+    <Toaster/>
   </SocketProvider>
 )
